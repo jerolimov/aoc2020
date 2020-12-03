abc0140a87facab3aef148ce8f5a19fdd2d1da91
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-12-04 00:58:43 +0100

    🦌 Day 3 part 2

--- day3/utils_test.go
+++ day3/utils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -15,13 +16,30 @@ func TestInput(t *testing.T) {
 		t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, 11)
 	}
 
-	slope := slope{
-		x: 3,
-		y: 1,
+	testcases := []struct {
+		slope         slope
+		expectedTrees int
+	}{
+		{slope{1, 1}, 2},
+		{slope{3, 1}, 7},
+		{slope{5, 1}, 3},
+		{slope{7, 1}, 4},
+		{slope{1, 2}, 2},
 	}
-	count := countTrees(area, slope)
-	if count != 7 {
-		t.Errorf("Unexpected number of trees: actual %v, expected %v", count, 7)
+
+	product := 1
+	for _, testcase := range testcases {
+		t.Run(fmt.Sprintf("Slope %d %d should found %d trees", testcase.slope.x, testcase.slope.y, testcase.expectedTrees), func(t *testing.T) {
+			actualTrees := countTrees(area, testcase.slope)
+			if actualTrees != testcase.expectedTrees {
+				t.Errorf("Unexpected number of trees for slope %d %d: actual %v, expected %v", testcase.slope.x, testcase.slope.y, actualTrees, testcase.expectedTrees)
+			}
+			product *= actualTrees
+		})
+	}
+
+	if product != 336 {
+		t.Errorf("Unexpected product of trees: actual %v, expected %v", product, 336)
 	}
 }
 
@@ -36,12 +54,29 @@ func TestMyInput(t *testing.T) {
 		t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, 323)
 	}
 
-	slope := slope{
-		x: 3,
-		y: 1,
+	testcases := []struct {
+		slope         slope
+		expectedTrees int
+	}{
+		{slope{1, 1}, 70},
+		{slope{3, 1}, 220},
+		{slope{5, 1}, 63},
+		{slope{7, 1}, 76},
+		{slope{1, 2}, 29},
 	}
-	count := countTrees(area, slope)
-	if count != 220 {
-		t.Errorf("Unexpected number of trees: actual %v, expected %v", count, 220)
+
+	product := 1
+	for _, testcase := range testcases {
+		t.Run(fmt.Sprintf("Slope %d %d should found %d trees", testcase.slope.x, testcase.slope.y, testcase.expectedTrees), func(t *testing.T) {
+			actualTrees := countTrees(area, testcase.slope)
+			if actualTrees != testcase.expectedTrees {
+				t.Errorf("Unexpected number of trees for slope %d %d: actual %v, expected %v", testcase.slope.x, testcase.slope.y, actualTrees, testcase.expectedTrees)
+			}
+			product *= actualTrees
+		})
+	}
+
+	if product != 2138320800 {
+		t.Errorf("Unexpected product of trees: actual %v, expected %v", product, 2138320800)
 	}
 }
