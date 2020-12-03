cfff57856ae26c25920ed34f9c53417d33174d79
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-12-04 00:40:45 +0100

    🎄 Day 3 part 1, add my test data

--- day3/utils_test.go
+++ day3/utils_test.go
@@ -24,3 +24,24 @@ func TestInput(t *testing.T) {
 		t.Errorf("Unexpected number of trees: actual %v, expected %v", count, 7)
 	}
 }
+
+func TestMyInput(t *testing.T) {
+	area, err := readData("my_input.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	// fmt.Printf("area:\n%v", area)
+	rows := area.rows()
+	if rows != 323 {
+		t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, 323)
+	}
+
+	slope := slope{
+		x: 3,
+		y: 1,
+	}
+	count := countTrees(area, slope)
+	if count != 220 {
+		t.Errorf("Unexpected number of trees: actual %v, expected %v", count, 220)
+	}
+}
