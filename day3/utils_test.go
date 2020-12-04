1df10970988d1ca237feb54291c7d17c784b3021
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-12-04 01:14:04 +0100

    🍪 Convert to a single test with multiple sub-testcases

--- day3/utils_test.go
+++ day3/utils_test.go
@@ -5,78 +5,76 @@ import (
 	"testing"
 )
 
-func TestInput(t *testing.T) {
-	area, err := readData("test_input.txt")
-	if err != nil {
-		t.Error(err)
-	}
-	// fmt.Printf("area:\n%v", area)
-	rows := area.rows()
-	if rows != 11 {
-		t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, 11)
-	}
-
+func TestFromFileData(t *testing.T) {
 	testcases := []struct {
-		slope         slope
-		expectedTrees int
+		filename     string
+		expectedRows int
+		slopeChecks  []struct {
+			slope         slope
+			expectedTrees int
+		}
+		expecedProduct int
 	}{
-		{slope{1, 1}, 2},
-		{slope{3, 1}, 7},
-		{slope{5, 1}, 3},
-		{slope{7, 1}, 4},
-		{slope{1, 2}, 2},
+		{
+			filename:     "test_input.txt",
+			expectedRows: 11,
+			slopeChecks: []struct {
+				slope         slope
+				expectedTrees int
+			}{
+				{slope{1, 1}, 2},
+				{slope{3, 1}, 7},
+				{slope{5, 1}, 3},
+				{slope{7, 1}, 4},
+				{slope{1, 2}, 2},
+			},
+			expecedProduct: 336,
+		},
+		{
+			filename:     "my_input.txt",
+			expectedRows: 323,
+			slopeChecks: []struct {
+				slope         slope
+				expectedTrees int
+			}{
+				{slope{1, 1}, 70},
+				{slope{3, 1}, 220},
+				{slope{5, 1}, 63},
+				{slope{7, 1}, 76},
+				{slope{1, 2}, 29},
+			},
+			expecedProduct: 2138320800,
+		},
 	}
 
-	product := 1
 	for _, testcase := range testcases {
-		t.Run(fmt.Sprintf("Slope %d %d should found %d trees", testcase.slope.x, testcase.slope.y, testcase.expectedTrees), func(t *testing.T) {
-			actualTrees := countTrees(area, testcase.slope)
-			if actualTrees != testcase.expectedTrees {
-				t.Errorf("Unexpected number of trees for slope %d %d: actual %v, expected %v", testcase.slope.x, testcase.slope.y, actualTrees, testcase.expectedTrees)
+		t.Run(fmt.Sprintf("Tests with file %s", testcase.filename), func(t *testing.T) {
+			area, err := readData(testcase.filename)
+			if err != nil {
+				t.Error(err)
 			}
-			product *= actualTrees
-		})
-	}
+			// fmt.Printf("area:\n%v", area)
 
-	if product != 336 {
-		t.Errorf("Unexpected product of trees: actual %v, expected %v", product, 336)
-	}
-}
-
-func TestMyInput(t *testing.T) {
-	area, err := readData("my_input.txt")
-	if err != nil {
-		t.Error(err)
-	}
-	// fmt.Printf("area:\n%v", area)
-	rows := area.rows()
-	if rows != 323 {
-		t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, 323)
-	}
-
-	testcases := []struct {
-		slope         slope
-		expectedTrees int
-	}{
-		{slope{1, 1}, 70},
-		{slope{3, 1}, 220},
-		{slope{5, 1}, 63},
-		{slope{7, 1}, 76},
-		{slope{1, 2}, 29},
-	}
-
-	product := 1
-	for _, testcase := range testcases {
-		t.Run(fmt.Sprintf("Slope %d %d should found %d trees", testcase.slope.x, testcase.slope.y, testcase.expectedTrees), func(t *testing.T) {
-			actualTrees := countTrees(area, testcase.slope)
-			if actualTrees != testcase.expectedTrees {
-				t.Errorf("Unexpected number of trees for slope %d %d: actual %v, expected %v", testcase.slope.x, testcase.slope.y, actualTrees, testcase.expectedTrees)
+			rows := area.rows()
+			if rows != testcase.expectedRows {
+				t.Errorf("Unexpected number of rows: actual %v, expected %v", rows, testcase.expectedRows)
 			}
-			product *= actualTrees
-		})
-	}
 
-	if product != 2138320800 {
-		t.Errorf("Unexpected product of trees: actual %v, expected %v", product, 2138320800)
+			product := 1
+			for _, slopeCheck := range testcase.slopeChecks {
+				slope := slopeCheck.slope
+				t.Run(fmt.Sprintf("Slope %d %d should found %d trees", slope.x, slope.y, slopeCheck.expectedTrees), func(t *testing.T) {
+					trees := countTrees(area, slope)
+					if trees != slopeCheck.expectedTrees {
+						t.Errorf("Unexpected number of trees for slope %d %d: actual %v, expected %v", slope.x, slope.y, trees, slopeCheck.expectedTrees)
+					}
+					product *= trees
+				})
+			}
+
+			if product != testcase.expecedProduct {
+				t.Errorf("Unexpected product of trees: actual %v, expected %v", product, testcase.expecedProduct)
+			}
+		})
 	}
 }
